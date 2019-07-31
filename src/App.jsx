17a0dd7fd6dc165cbcbe0d7f5beb96808e9b3442
onoroff <adam.grunke@gmail.com>
2019-07-31 14:17:07 -0700

    Initial Commit. Mock up of app with single User Post complete.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import UserPost from './UserPost';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Cycle Through Users</h1>
+      <UserPost/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/UserPost.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UserPost = () => {
+
+    return(
+        <div className="userPost">
+            <h3>User ID: 3</h3>
+            <h4>Title: amazing title here</h4>
+            <p>this is a post this is a post this is a post this is a post</p>
+        </div>
+    )
+}
+
+export default UserPost;
